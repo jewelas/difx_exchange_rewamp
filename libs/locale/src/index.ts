340311ddf9ed896b3f1005bd5f578854a90e1e25
Man Le <man.le@diginex.com>
2022-04-04 14:50:19 +0700

    remove redundant files

--- libs/locale/src/index.ts
+++ libs/locale/src/index.ts
@@ -3,6 +3,7 @@ import en from './en.json';
 
 let Language = en;
 if (typeof window !== "undefined") {
+    console.log('ddddddddd')
     const lang = window.localStorage.getItem('lang');
     switch (lang) {
         case 'en': Language = en;

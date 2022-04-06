3da8cc2acc9a0602d20e5d799d0712c0c48f8a2e
Man Le <man.le@diginex.com>
2022-04-06 20:11:23 +0700

    remove rendundant files

--- libs/shared/api/captcha.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosResponse } from 'axios';
-import { PairType } from '../type/Pair';
-import instance from './index';
-
-export function getToken() {
-    return instance.post<null, AxiosResponse<string[]>>('https://recaptcha.net/recaptcha/api2/reload?k=6Lc_bTocAAAAAEdBFuwabJr5of-6kdhfZW4aag3E');
-}

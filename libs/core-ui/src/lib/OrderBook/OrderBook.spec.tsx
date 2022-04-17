5800f8ff184c823c791183045815a9d3daa253fc
Man Le <man.le@diginex.com>
2022-04-17 23:08:16 +0700

    built layout for Orderbook component

--- libs/core-ui/src/lib/OrderBook/OrderBook.spec.tsx
+++ libs/core-ui/src/lib/OrderBook/OrderBook.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import {OrderBook} from '.';
 
-describe('Header', () => {
+describe('OrderBook', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<OrderBook />);
     expect(baseElement).toBeTruthy();

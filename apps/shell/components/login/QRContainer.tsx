1d21d7b65d02461aa472f045e75c5469abcc1551
milan <milan.sha@difx.io>
2022-05-09 14:35:39 +0400

    qr login -initial

---
+++ apps/shell/components/login/QRContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import QRScanImage from "./svg/QRScanImage";
+
+export default function QRContainer() {
+  return (
+    <QRScanImage />
+  )
+}
--- apps/shell/pages/login/index.tsx
+++ apps/shell/pages/login/index.tsx
@@ -5,11 +5,19 @@ import Link from "next/link";
 import AppLayout from "..";
 import PageStyled from "./styled";
 import LoginForm from "./../../components/login/LoginForm";
+import QRContainer from "./../../components/login/QRContainer";
+import { useEffect, useState } from "react";
 
 /* eslint-disable-next-line */
 export interface LoginPageProps {}
 
 export function LoginPage(props: LoginPageProps) {
+  const [QRLogin, setQRLogin] = useState<boolean>(false)
+
+  useEffect(()=>{
+    console.log(QRLogin)
+  },[QRLogin])
+
   return (
     <AppLayout>
       <PageStyled>
@@ -25,27 +33,38 @@ export function LoginPage(props: LoginPageProps) {
               </div>
             </div>
 
-            <LoginForm />
+            
+            {
+              QRLogin === true ?
+                <QRContainer /> 
+              : 
+                <>
 
-            <div className="left-right">
-              <div className="left">
-                <Typography level="B1">
-                  {t("signin.new_account")}{" "}
-                  <Link href="/register">{t("common.register")}</Link>
-                </Typography>
-              </div>
-              <div className="right.forgot-pass">
-                <Typography level="B1">
-                  <Link href="/forgot">{t("common.forgot_password")}</Link>
-                </Typography>
-              </div>
-            </div>
-            <div className="or">
-              <div>Or</div>
-            </div>
+                  <LoginForm />
+
+                  <div className="left-right">
+                    <div className="left">
+                      <Typography level="B1">
+                        {t("signin.new_account")}{" "}
+                        <Link href="/register">{t("common.register")}</Link>
+                      </Typography>
+                    </div>
+                    <div className="right.forgot-pass">
+                      <Typography level="B1">
+                        <Link href="/forgot">{t("common.forgot_password")}</Link>
+                      </Typography>
+                    </div>
+                  </div>
+                  <div className="or">
+                    <div>Or</div>
+                  </div>
+                </>
+            }
+
             <Button
               htmlType="submit"
               className="sign-in-qrcode"
+              onClick={()=>setQRLogin(!QRLogin)}
             >
               <Icon.QRCodeIcon />
               <span>{t("signin.login_qr")}</span>

7f5c8f99e89969d2f8ad19bd4458b7600d1d3110
milan <milan.sha@difx.io>
2022-05-10 18:26:44 +0400

    qr generation complete

--- apps/shell/components/login/QRContainer.tsx
+++ apps/shell/components/login/QRContainer.tsx
@@ -1,31 +1,46 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { QRContainerStyled } from './QRContainer.style'
 import { QRCodeSVG } from 'qrcode.react';
 import { 
-  useHttpGetByEvent,
+  useHttpPost,
+  useFingerprint,
   API_ENDPOINT
  } from '@difx/shared';
 
 export default function QRContainer() {
+  
+  const [ qrToken, setQRToken ] = useState()
+  const { getFingerprint } = useFingerprint()
 
   const onSuccess = useCallback((response)=>{
-    console.log(response)
+    const { token } = response.data.data
+    setQRToken(token)
   },[])
 
   const onError = useCallback((error)=>{
     console.log(error)
   },[])
 
-  const { mutate: getQr } = useHttpGetByEvent({onSuccess, onError, endpoint:API_ENDPOINT.GET_LOGIN_QR});
+  const { mutate: getQr } = useHttpPost({onSuccess, onError, endpoint:API_ENDPOINT.GET_LOGIN_QR});
+
+  const getNewQr = async() => {
+    const fingerprint = await getFingerprint()
+    const reqData = { 
+      device_type: "web",
+      device_id: fingerprint,
+      identifier: fingerprint
+    }
+    getQr(reqData)
+  }
 
   useEffect(()=>{
-    getQr(null)
+    getNewQr()
   },[])
 
   return (
     <QRContainerStyled>
       <QRCodeSVG
-       value="https://difx.com/"
+       value={qrToken}
       />
     </QRContainerStyled>
   )

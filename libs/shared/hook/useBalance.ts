3151cad01b15c254703cb24238de93cad62f988b
milan <milan.sha@difx.io>
2022-05-20 16:55:25 +0400

    useBalance added

---
+++ libs/shared/hook/useBalance.ts
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useHttpGetByEvent } from "..";
+import { Balance } from './../type/Balance'
+import { API_ENDPOINT, QUERY_KEY } from "../constants"
+import { isLoggedInAtom, userBalanceAtom } from "../atom/index"
+import { useAtom } from "jotai";
+import { AxiosResponse } from "axios";
+
+
+export function useBalance() {
+  const [isLoggedIn] = useAtom(isLoggedInAtom)
+  const [userBalance, setUserBalance] = useAtom(userBalanceAtom )
+
+  const onSuccess = (response: AxiosResponse) => {
+    const { data } = response
+    console.log(response)
+    setUserBalance(data)
+  }
+  
+  const { mutate: getBalance } = useHttpGetByEvent<null, any>({ onSuccess, endpoint: API_ENDPOINT.GET_BALANCE });
+
+  useEffect(() => {
+    if(isLoggedIn){
+      getBalance(null)
+    }else{
+      setUserBalance([])
+    }
+  }, [isLoggedIn])
+
+
+  return {
+    userBalance,
+    setUserBalance
+  }
+}

5cd2e7f349db09ff064deb76fcfe868ff6b765d4
Man Le <manle2012@ymail.com>
2022-05-23 13:19:47 +0700

    handle router.push for default pair

--- apps/shell/pages/exchange/index.page.tsx
+++ apps/shell/pages/exchange/index.page.tsx
@@ -1,6 +1,7 @@
-import { DEFAULT_PAIR, STORE_KEY } from "@difx/constants";
-import { useLocalStorage } from "@difx/shared";
+import { API_ENDPOINT, DEFAULT_PAIR, QUERY_KEY, STORE_KEY } from "@difx/constants";
 import { Loading } from "@difx/core-ui";
+import { useHttpGet, useLocalStorage } from "@difx/shared";
+import { isEmpty } from "lodash";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import AppLayout from "../index.page";
@@ -8,12 +9,29 @@ export function ExchangeDefault() {
   const router = useRouter();
 
   const { value: lastPair } = useLocalStorage(STORE_KEY.LAST_PAIR, null);
+  const { data: resData } = useHttpGet<null, any>(QUERY_KEY.PAIRS, API_ENDPOINT.GET_PAIRS, null);
+
   useEffect(() => {
-    router.push(`/exchange/${lastPair ? lastPair : DEFAULT_PAIR}`);
-  });
+
+    if (lastPair) {
+      const { spot } = resData;
+      if (!isEmpty(spot)) {
+        if (spot.find(e => e.symbol === lastPair)) router.push(`/exchange/${lastPair}`);
+        else router.push(`/exchange/${spot[0].symbol}`);
+      }
+
+    } else if (resData) {
+      const { spot } = resData;
+      if (!isEmpty(spot)) router.push(`/exchange/${spot[0].symbol}`);
+      else router.push(`/exchange/${lastPair || DEFAULT_PAIR}`);
+
+    } else router.push(`/exchange/${DEFAULT_PAIR}`);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resData]);
   return (
     <AppLayout>
-      <div style={{ position:'absolute', width:'100%', height: '100%' }}>
+      <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
         <Loading style={{ height: '100%' }} />
       </div>
     </AppLayout>

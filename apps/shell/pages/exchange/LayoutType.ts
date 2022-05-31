d5b5d3f82d96ec89af665bcc2a812cb277589310
Man Le <manle2012@ymail.com>
2022-05-31 11:04:45 +0700

    increase width for Trade Info in Pro layout

--- apps/shell/pages/exchange/LayoutType.ts
+++ apps/shell/pages/exchange/LayoutType.ts
@@ -81,18 +81,18 @@ export function getLayoutType(type: 'default' | 'compact' | 'pro', isStatic?: bo
       i: "pair-info",
       x: 0,
       y: 0,
-      w: 15,
+      w: 14,
       h: 1,
       minH: 1,
       maxH: 1,
       static: isStaticWidgets,
     },
-    { i: "chart", x: 0, y: 1, w: 15, h: 8, static: isStaticWidgets },
+    { i: "chart", x: 0, y: 1, w: 14, h: 8, static: isStaticWidgets },
 
     // Col 2
     {
       i: "order-book",
-      x: 15,
+      x: 14,
       y: 0,
       w: 5,
       h: 17,
@@ -101,14 +101,14 @@ export function getLayoutType(type: 'default' | 'compact' | 'pro', isStatic?: bo
     },
 
     // Col 3
-    { i: "trade-info", x: 20, y: 0, w: 4, h: 17, static: isStaticWidgets },
+    { i: "trade-info", x: 19, y: 0, w: 5, h: 17, static: isStaticWidgets },
 
 
     // Row 2 - Col 1
     { i: "pair-search", x: 0, y: 9, w: 5, h: 8, static: isStaticWidgets },
 
     // Row 2 - Col 2
-    { i: "place-order", x: 5, y: 9, w: 10, h: 8, static: isStaticWidgets },
+    { i: "place-order", x: 5, y: 9, w: 9, h: 8, static: isStaticWidgets },
 
     // Row 3
     { i: "report", x: 0, y: 17, w: 24, h: 7, static: isStaticWidgets },
